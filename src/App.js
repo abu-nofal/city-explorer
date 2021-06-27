3341fc439d7669bc6aa931d5ab63cbee655c34c1
abu-nofal <anofal719@gmail.com>
2021-06-27 16:38:28 +0300

    push the branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Card } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super();
+    this.state={
+      displayName:' ',
+      latitude:' ', 
+      longitude: ' '  
+    }
+  }
+  HandleDisplayName=(e)=>{
+    this.setState({
+      displayName:e.target.value
+    })
+  }
+  SubmitForm=async (e)=>{
+    e.preventDefault();// to not relode the page
+    let axiResponse=await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.54c5bcb87e24270823ee985ff91c6f9c&city=${this.state.displayName} &format=json`)
+
+
+    // console.log(axiResponse.data)
+    this.setState({ //to change the state of the propertes ,and change them with 
+      displayName:axiResponse.data[0].display_name,
+      latitude:axiResponse.data[0].lat,
+      longitude : axiResponse.data[0].lon
+
+
+    })
+    
+
+
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={(e)=>{this.SubmitForm(e)}}>
+          <input type='text' onChange={(e)=>{this.HandleDisplayName(e)}}/> 
+          <button >Explore!</button>
+        </form>
+        
+
+
+              <Card style={{ width: '18rem' }}>
+        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+        <Card.Body>
+          <Card.Title>{this.state.displayName}</Card.Title>
+          <Card.Text>
+           latitude:   {this.state.latitude}<br/>
+          longitude:  {this.state.longitude }
+          </Card.Text>
+          
+        </Card.Body>
+      </Card>
+      
+          {/* <h2>Name:  {this.state.displayName}</h2> */}
+          
+
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
