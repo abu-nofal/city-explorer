b5e3b9e8b6dea799dc228e0adf9ddac34733e42e
abu-nofal <anofal719@gmail.com>
2021-06-29 20:57:21 +0300

    the lab 8 done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Card } from 'react-bootstrap';
+import Weather from './components/Weather';
+import Movies from './components/Movies';
 
 
 class App extends Component {
@@ -10,9 +12,10 @@ class App extends Component {
       displayName:' ',
       latitude:' ', 
       longitude: ' ' ,
-      
+      weatherData:[],
       show : false,
-      error:''
+      error:'',
+      moviesData:[]
     }
   }
   HandleDisplayName=(e)=>{
@@ -23,17 +26,29 @@ class App extends Component {
   SubmitForm=async (e)=>{
     e.preventDefault();// to not relode the page
     try{
+
+      // ======================== for loction qi 
     let axiResponse=await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.54c5bcb87e24270823ee985ff91c6f9c&city=${this.state.displayName} &format=json`);
-
+    
+    let lat=axiResponse.data[0].lat;
+    let lon=axiResponse.data[0].lon;
+      let KeyLocal=process.env.REACT_APP_BACKEND_URL
+    // ============================for weather data 
+    let axiosWeaterResponce=await axios.get(`${KeyLocal}/weather?lat=${lat}&lon=${lon}`)
+    // console.log(axiosWeaterResponce.date)
   
-
+  // =========================for movies 
+    let axiosMoviesResponce=await axios.get(`${KeyLocal}/movies?city=${this.state.displayName}`)
+    // console.log(axiosMoviesResponce.data)
    
     this.setState({ //to change the state of the propertes ,and change them with 
       displayName:axiResponse.data[0].display_name,
       latitude:axiResponse.data[0].lat,
       longitude : axiResponse.data[0].lon,
+      weatherData: axiosWeaterResponce.data,
       show :!this.state.show,
-      error:''
+      error:'',
+      moviesData:axiosMoviesResponce.data
            })
           }
           catch{
@@ -67,6 +82,19 @@ class App extends Component {
       </Card>  
       
     }
+    {
+      this.state.weatherData.map(value=>{
+        return  <Weather desc={value.description} date={value.date} />
+      })
+    }
+    {
+      this.state.moviesData.map(value=>{
+        return  <Movies average_votes={value.average_votes} image_url={value.image_url} 
+        popularity={value.popularity}   released_on={value.released_on}  total_votes={value.total_votes}/>
+      })
+
+    }
+   
     {
     <p>{this.state.error}</p>
     }
